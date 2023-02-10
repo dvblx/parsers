4382a85358e20c7bc2ca5136648bc5fba7f810e5
dvblx <artmel03@gmail.com>
2023-02-10 17:15:02 +0300

    Исправил некоторые косяки

--- crypto_parser_python_binance.py
+++ crypto_parser_python_binance.py
@@ -1,20 +1,26 @@
 from binance import AsyncClient
 import asyncio
-import json
 
 
 async def main():
     client = await AsyncClient.create()
-    needed = 860
-    while True:
-        req1 = await client.get_products()
-        actual_cost = float(req1['data'][needed]['c'])  # Для получения текущей стоимости фьючерса
-        req2 = await client.get_historical_klines(symbol='XRPUSDT', interval='1h', limit=1)
-        max_cost = float(req2[0][2])  # Для получения максимума за час
-        cost_difference = (max_cost - actual_cost) / actual_cost * 100
-        if round(cost_difference) == 1:
-            print(f"Цена упала на 1%.\nМаксимальная цена за последний час: {max_cost}\nТекущая цена: {actual_cost}")
-        print(f"Текущая стоимость: {actual_cost}, максимальная стоимость: {max_cost}, Разница: {cost_difference}")
+    needed = -1
+    req1 = await client.get_products()
+    all_symbs = req1['data']
+    for i in range(len(all_symbs)):
+        if all_symbs[i]['s'] == 'XRPUSDT':
+            needed = i
+            break
+    if needed != -1:
+        while True:
+            req1 = await client.get_products()
+            actual_cost = float(req1['data'][needed]['c'])  # Для получения текущей стоимости фьючерса
+            req2 = await client.get_historical_klines(symbol='XRPUSDT', interval='1h', limit=1)
+            max_cost = float(req2[0][2])  # Для получения максимума за час
+            cost_difference = (max_cost - actual_cost) / actual_cost * 100
+            if cost_difference == 1:
+                print(f"Цена упала на 1%.\nМаксимальная цена за последний час: {max_cost}\nТекущая цена: {actual_cost}")
+            #print(f"Текущая стоимость: {actual_cost}, максимальная стоимость: {max_cost}, Разница: {cost_difference}")
     await client.close_connection()
 
 
--- crypto_parser_websocket.py
+++ crypto_parser_websocket.py
@@ -4,8 +4,9 @@ import json
 
 
 async def main():
-    url1 = "wss://stream.binance.com:9443/stream?streams=xrpusdt@kline_1h"
-    url2 = "wss://stream.binance.com:9443/stream?streams=xrpusdt@trade"
+    futures_name = 'XRPUSDT'.lower()
+    url1 = f"wss://stream.binance.com:9443/stream?streams={futures_name}@kline_1h"
+    url2 = f"wss://stream.binance.com:9443/stream?streams={futures_name}@trade"
     async with websockets.connect(url2) as client:
         while True:
             data2 = json.loads(await client.recv())['data']
@@ -14,16 +15,13 @@ async def main():
             async with websockets.connect(url1) as client1:
                 max_cost = float(json.loads(await client1.recv())['data']['k']['h'])
             cost_difference = (max_cost - actual_cost) / actual_cost * 100
-            if round(cost_difference) == 1:
+            if cost_difference == 1:
                 print(f"Цена упала на 1%.\nМаксимальная цена за последний час: {max_cost}\nТекущая цена: {actual_cost}")
-            print(f"Время: {actual_time}, текущая стоимость: {actual_cost}, максимальная стоимость за час: {max_cost}\n"
-                  f"Разность стоимости в процентах: {cost_difference}")
+            #print(f"Время: {actual_time}, текущая стоимость: {actual_cost}, максимальная стоимость за час: {max_cost}\n"
+            #      f"Разность стоимости в процентах: {cost_difference}")
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-
-
-
 
