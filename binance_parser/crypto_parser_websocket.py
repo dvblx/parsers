4ab73c5329f1f28b16fe534d75b102f16c05ea16
dvblx <artmel03@gmail.com>
2023-03-06 20:31:25 +0300

    Сбор данных со строительных магазинов с сохранением в json

--- binance_parser/crypto_parser_websocket.py
+++ binance_parser/crypto_parser_websocket.py
@@ -15,13 +15,12 @@ async def main():
             async with websockets.connect(url1) as client1:
                 max_cost = float(json.loads(await client1.recv())['data']['k']['h'])
             cost_difference = (max_cost - actual_cost) / actual_cost * 100
-            if cost_difference == 1:
+            if cost_difference >= 1:
                 print(f"Цена упала на 1%.\nМаксимальная цена за последний час: {max_cost}\nТекущая цена: {actual_cost}")
-            #print(f"Время: {actual_time}, текущая стоимость: {actual_cost}, максимальная стоимость за час: {max_cost}\n"
-            #      f"Разность стоимости в процентах: {cost_difference}")
+            print(f"Время: {actual_time}, текущая стоимость: {actual_cost}, максимальная стоимость за час: {max_cost}\n"
+                  f"Разность стоимости в процентах: {cost_difference}")
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-
